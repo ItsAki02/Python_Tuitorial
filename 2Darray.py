93ba981fec56325408926cd73990c9fc4bd45e0b
itsAki02 <akanksha021817@gmail.com>
2024-09-21 13:33:36 +0530

    2D arrays

---
+++ 2Darray.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+#creating 2-D array 
+# arr = np.array([[1,2],[3,4]])
+# print(arr)             #matrix of order 2x2
+
+arr = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(arr)
---
+++ Array_Attributes.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+#1. Shape Attribute
+#displays the shape
+''''a = np.array([[1,2,3],[4,5,6]])
+print(a.shape)
+print(a)
+#change the shape'''
+'''a.shape = (3,2) #change it to 3*2 matrix
+print(a)'''
+
+#2.reshape()
+#to resize the existing array and storing the result in a new array object
+'''a = np.array([[1,2,3],[4,5,6]])
+b = a.reshape(3,2)
+print(b)'''
