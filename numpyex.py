b6fad10793568aa723f2e497b57d0d30557592f1
itsAki02 <akanksha021817@gmail.com>
2024-09-21 12:57:17 +0530

    numpy array creation

---
+++ numpyex.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+ls = [1,2,3,4,5]
+np_ls = np.array(ls)
+# print(np_ls)
+# print(type(np_ls))
+
+# squared = np_ls**2
+# print(squared)
+
+#creating simple array
+# arr =  np.array([1,2,3])
+# print(arr)
+
+#Array with diff datatypes
+arr = np.array([2,'hi',3.5,6])
+print(arr)
