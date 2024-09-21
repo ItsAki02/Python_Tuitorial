8f79dd2d6a8b8cb99e5f772c5df83ddae9d9c797
itsAki02 <akanksha021817@gmail.com>
2024-09-21 13:40:34 +0530

    array attributes

--- Array_Attributes.py
+++ Array_Attributes.py
@@ -14,3 +14,18 @@ print(a)'''
 '''a = np.array([[1,2,3],[4,5,6]])
 b = a.reshape(3,2)
 print(b)'''
+
+#3. ndim
+#display dimensions of the array object
+a= np.arange(24)
+'''print(a)
+print(np.ndim(a))'''
+#change the dimensions of a
+b = a.reshape(6,4)
+print(b)
+print('dimension of a is : ', np.ndim(a))
+print('dimension of b is : ',np.ndim(b))
+c = a.reshape(3,4,2)
+print(c)
+print('dimension of c is: ',np.ndim(c))
+print('shape of c is ',np.shape(c))
