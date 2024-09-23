a261bd99dc5563081c23d8df553a85df8610b390
itsAki02 <akanksha021817@gmail.com>
2024-09-23 13:21:37 +0530

    Generating Random Numbers

---
+++ NpRandomNos.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+#numpy.random.rand()- returns an array of given shape filled with random values
+
+x= np.random.rand(5)
+print(x)
+
+#generating random nos. with specific sape
+x = np.random.rand(5,2)
+print(x)
