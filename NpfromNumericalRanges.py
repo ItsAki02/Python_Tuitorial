03d7494220fe36f4a80980813dcfc06fb290e6cf
itsAki02 <akanksha021817@gmail.com>
2024-09-23 13:17:38 +0530

    Array from Numerical ranges

---
+++ NpfromNumericalRanges.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#create array from arange()
+a = np.arange(5)
+print(a)
+
+#create array of floats
+a = np.arange(5, dtype=float)
+print(a)
+
+#create array in given range with steps
+a = np.arange(1, 10, 2)
+print(a)
